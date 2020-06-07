b939ebb105fdde917e2fad9869b01797999451bd
maleckyy <kupferpatryk@gmail.com>
2020-06-07 16:01:30 +0200

    klasa klienta ale pewnie to zmienie

---
+++ com/maleckyyautokomis/Customer.java
@@ -0,0 +1,10 @@
+package com.maleckyyautokomis;
+
+public class Customer {
+        public String customerName;
+        public Double kasaKlienta;
+        public boolean sprawne;//czy samochod w pełni sprawny
+    //ulubione marki
+        public String favMarka1;
+        public String favMarka2;
+}
